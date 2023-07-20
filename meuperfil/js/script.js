b365784160acca02b98c11c070b3a00233d53889
rita_@Almeida <rita_aja92@hotmail.com>
2023-07-19 23:02:30 -0300

    Aplicando os conhecimentos de HTML e CSS vistos na aula sde hoje

--- meuperfil/js/script.js
+++ meuperfil/js/script.js
@@ -33,6 +33,8 @@ let nomeOK = false
 let emailOK = false
 let assuntoOK = false
 let mapa = document.querySelector("#mapa")
+let about = document.querySelector('#about')
+let areaProfile = document.querySelector('#area')
 
 nome.style.width = "100%"
 email.style.width = "100%"
@@ -96,3 +98,37 @@ function mapaNormal() {
     mapa.style.height = "250px"
 }
     
+async function getApiGithub() {
+    try {
+        const dataProfile = await fetch('https://github.com/RitaAlmeidah')
+        const profile = await dataProfile.json()
+
+        let content = `
+            <img src="${profile.avatar_url}" alt="Foto do Perfil do Github - ${profile.name}">
+            <article class="section_content">
+                <h1>Who am I?</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam odit voluptates incidunt
+                    inventore rem error harum blanditiis accusamus vitae, minus fugit consequatur? Dolorum maiores magni
+                    deleniti modi sit laudantium totam!
+                </p>
+
+                <div class="section_github flex">
+                    <a class="btn" href="${profile.html_url}">Github</a>
+                    <p>${profile.followers} Seguidores</p>
+                    <p>${profile.public_repos} Repositórios</p>
+                </div>
+            </article>
+        `
+        about.innerHTML += content
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+function changeTheme() {
+    document.body.classList.toggle('dark-theme')
+}
+
+getApiGithub()
